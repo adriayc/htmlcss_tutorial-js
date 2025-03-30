7265b3f13eb8882959d6c1ae597d29a2f77b26a3
Adriano Ayala <adriano.ayc@gmail.com>
2025-03-29 22:34:57 -0400

    Sidebar Functionality

--- section_32-portfolio_project_with_css_grid/portfolio-project/js/app.js
+++ section_32-portfolio_project_with_css_grid/portfolio-project/js/app.js
@@ -1,1 +1,12 @@
-console.log('portfolio project');
+/* ***** sidebar ***** */
+const navBtn = document.querySelector('#nav-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('#sidebar');
+// show sidebar
+navBtn.addEventListener('click', () => {
+  sidebar.classList.add('show-sidebar');
+});
+// close sidebar
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+});
